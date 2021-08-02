3c44fa221c312c987cdf6fc59ea584cff11b11cd
yinhy <1061929244@qq.com>
2021-08-02 17:21:29 +0800

    feature：增加饼状图

--- src/data/chart3.js
+++ src/data/chart3.js
@@ -1,5 +1,5 @@
 import * as echarts from "echarts";
-let chart1 = function (chartDom) {
+let chart3 = function (chartDom) {
     var myChart = echarts.init(chartDom);
     var option = null;
     var datas = [
@@ -83,4 +83,4 @@ let chart1 = function (chartDom) {
         myChart.setOption(option);
     }
 };
-export default chart1
+export default chart3
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,45 @@
       <div class="leftContent">
         <div class="chart1">
           <div class="chartTitle">街道空置率</div>
+          <div class="t_line_box">
+            <i class="t_l_line"></i>
+            <i class="l_t_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="t_r_line"></i>
+            <i class="r_t_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="b_l_line"></i>
+            <i class="l_b_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="b_r_line"></i>
+            <i class="r_b_line"></i>
+          </div>
           <div id="chart1"></div>
         </div>
-        <div class="chart2"></div>
-        <div class="chart3"></div>
+        <div class="chart2 mt20"></div>
+        <div class="chart3 mt20">
+          <div class="chartTitle">电梯</div>
+          <div class="t_line_box">
+            <i class="t_l_line"></i>
+            <i class="l_t_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="t_r_line"></i>
+            <i class="r_t_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="b_l_line"></i>
+            <i class="l_b_line"></i>
+          </div>
+          <div class="t_line_box">
+            <i class="b_r_line"></i>
+            <i class="r_b_line"></i>
+          </div>
+          <div id="chart3"></div>
+        </div>
       </div>
       <div class="centerContent">
         <div class="chart4"></div>
@@ -35,6 +70,7 @@
 </template>
 <script>
 import chart1 from "../data/chart1";
+import chart3 from "../data/chart3";
 // import * as echarts from "echarts";
 export default {
   name: "Home",
@@ -47,8 +83,10 @@ export default {
   },
   methods: {
     init() {
-      var chartDom = document.getElementById("chart1");
-      chart1(chartDom);
+      var chartDom1 = document.getElementById("chart1");
+      var chartDom3 = document.getElementById("chart3");
+      chart1(chartDom1);
+      chart3(chartDom3);
     },
   },
 };
@@ -83,7 +121,7 @@ export default {
   }
   .content {
     background: #000433;
-    padding: 10px 20px;
+    padding: 15px 20px;
     overflow: hidden;
     width: 100%;
     box-sizing: border-box;
@@ -101,13 +139,82 @@ export default {
       float: right;
     }
     .chartTitle {
+      line-height: 33px;
+      position: absolute;
+      color: #92e9ff;
+      font-size: 16px;
+      box-sizing: border-box;
+      z-index: 1000;
+      width: 100%;
       text-align: center;
+    }
+    .t_line_box {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      i {
+        background-color: #92e9ff;
+        box-shadow: 0px 0px 10px #92e9ff;
+        position: absolute;
+      }
+      .t_l_line {
+        height: 5px;
+        width: 26px;
+        left: -3px;
+        top: -3px;
+      }
+      .l_t_line {
+        width: 5px;
+        height: 24px;
+        left: -3px;
+        top: -3px;
+      }
+      .t_r_line {
+        height: 5px;
+        width: 26px;
+        right: -3px;
+        top: -3px;
+      }
+      .r_t_line {
+        width: 5px;
+        height: 24px;
+        right: -3px;
+        top: -3px;
+      }
+      .l_b_line {
+        width: 5px;
+        height: 24px;
+        left: -3px;
+        bottom: -3px;
+      }
+      .b_l_line {
+        height: 5px;
+        width: 26px;
+        left: -3px;
+        bottom: -3px;
+      }
+      .r_b_line {
+        width: 5px;
+        height: 24px;
+        right: -3px;
+        bottom: -3px;
+      }
+      .b_r_line {
+        height: 5px;
+        width: 26px;
+        right: -3px;
+        bottom: -3px;
+      }
     }
     .chart1 {
       height: 300px;
-      border: 1px solid #92e9ff;
+      border: 1px solid #2c58a6;
+      box-shadow: 0 0 10px #2c58a6;
+      border-radius: 6px;
+      position: relative;
+
       #chart1 {
-        height: 220px;
+        height: 300px;
       }
     }
     .chart2 {
@@ -116,7 +223,13 @@ export default {
     }
     .chart3 {
       height: 300px;
-      background: rgb(145, 82, 82);
+      border: 1px solid #2c58a6;
+      box-shadow: 0 0 10px #2c58a6;
+      border-radius: 6px;
+      position: relative;
+      #chart3 {
+        height: 300px;
+      }
     }
     .chart4 {
       height: 300px;
